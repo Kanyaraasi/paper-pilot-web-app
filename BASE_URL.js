71493045733a3d33c28fcbf530f02a743fd724ab
SanjaySargam <sargamsanjaykumar@gmail.com>
2025-05-03 14:04:34 +0530

    move to prd

--- BASE_URL.js
+++ BASE_URL.js
@@ -1,2 +1,2 @@
-// const BASE_URL ='https://paper-pilot-backend.onrender.com'
-export const BASE_URL = 'http://localhost:5000'
+const BASE_URL ='https://paper-pilot-backend.onrender.com'
+// export const BASE_URL = 'http://localhost:5000'

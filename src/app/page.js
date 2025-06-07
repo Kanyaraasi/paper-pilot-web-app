a269a9b3924d2af6aaed93a36cc663293d24329d
Madhu Lagishetty <madhu@example.com>
2025-06-08 02:05:31 +0530

    header:- shadow fixed header:-admin dropdown login and register form reset()

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 
 // import HomePage from './Components/HomePage'
-import HomePage from "./Components/login/Login";
+import HomePage from "./Components/login/HomePage";
 
 export default function Home() {
   return (

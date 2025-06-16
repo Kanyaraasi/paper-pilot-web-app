3c28c3362ef7a6daf56b3b0b4ff8d6f82ff608c0
SanjaySargam <sargamsanjaykumar@gmail.com>
2025-06-16 10:52:39 +0530

    prod

--- BASE_URL.js
+++ BASE_URL.js
@@ -1,2 +1,4 @@
 export const BASE_URL ='https://paper-pilot-backend.vercel.app'
 // export const BASE_URL = 'http://localhost:5000'
+export const DEV = 'https://paper-pilot-backend-git-dev-kanyaraasis-projects.vercel.app'
+export const PROD = 'https://paper-pilot-backend-tau.vercel.app'
--- src/app/Components/login/HomePage.jsx
+++ src/app/Components/login/HomePage.jsx
@@ -56,7 +56,7 @@ function HomePage() {
     if (roleType === "admin") {
         window.location.href = "/CreateUser";
       } else if (roleType === "teacher") {
-        window.location.href = "/GradeSelector";
+        window.location.href = "/TestDashboard";
       }
     // Show toast message for all users who click the button
     // setToastMessage("Please login to get started!");

848657ed296bc66d673f961c244b1bebc8ce52af
SanjaySargam <sargamsanjaykumar@gmail.com>
2025-06-21 17:05:35 +0530

    dev url

--- BASE_URL.js
+++ BASE_URL.js
@@ -1,4 +1,3 @@
-export const BASE_URL ='https://paper-pilot-backend.vercel.app'
 // export const BASE_URL = 'http://localhost:5000'
-export const DEV = 'https://paper-pilot-backend-git-dev-kanyaraasis-projects.vercel.app'
-export const PROD = 'paper-pilot-backend-git-main-kanyaraasis-projects.vercel.app'
+export const BASE_URL = 'https://paper-pilot-backend-git-dev-kanyaraasis-projects.vercel.app'
+// export const BASE_URL = 'https://paper-pilot-backend-git-main-kanyaraasis-projects.vercel.app'
